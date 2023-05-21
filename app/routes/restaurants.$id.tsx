16f108e0ff1df1aca2008cd0f1e5c7d4dd93b28b
Frank van der Heijden <frank.boekanier@gmail.com>
2023-05-21 18:43:23 +0200

    Add count for total pos/neg reviews

--- app/routes/restaurants.$id.tsx
+++ app/routes/restaurants.$id.tsx
@@ -131,6 +131,11 @@ export default function Restaurant() {
     }
   }
 
+  const [positiveCount, negativeCount] = restaurant.reviews.reduce(([pos, neg], { sentiment }) => {
+    if (sentiment == null) return [pos, neg];
+    return sentiment ? [pos + 1, neg] : [pos, neg + 1];
+  }, [0, 0]);
+
   return (
     <div>
       <div className="flex flex-col items-center justify-center">
@@ -144,12 +149,16 @@ export default function Restaurant() {
           <div className="mt-2 flex">
             <textarea id="comment" name="comment" rows={3} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"></textarea>
           </div>
-          <div className="mt-6 flex items-center justify-end gap-x-6">
+          <div className="mt-4 flex items-center justify-center gap-x-6">
             <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
           </div>
           <input type="hidden" name="action" value="add-review" />
         </form>
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Reviews</h2>
+        <h2 className="mt-8 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Reviews</h2>
+        <div className="flex flex-row text-center min-w-[400px] mt-4 mb-4">
+          <div className="basis-1/2">😄 {positiveCount}</div>
+          <div className="basis-1/2">😐 {negativeCount}</div>
+        </div>
         <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
           {restaurant.reviews.map((review) => (
             <div key={review.id} className="border-b last:border-0 border-gray-200 py-4">
